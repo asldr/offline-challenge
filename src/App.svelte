1abe97c12938037f447bd9eaf86cf2d0045beab0
asldr <asldr@users.noreply.github.com>
2020-04-18 19:21:20 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<article class="stack">
+    <h1>Hey Sportskanone!</h1>
+    <p>Schön, dass du den Weg zu uns gefunden hast. Du kannst dich hier anmelden und direkt an einer der zahlreichen Herausforderungen teilnehmen.</p>
+    <button>Klar, ich mache mit!</button>
+  </article>
+</main>
